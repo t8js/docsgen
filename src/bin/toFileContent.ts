501425fb97f057f394152f11cdc88ae84c2e2148
axtk <axtkx@inbox.ru>
2025-10-19 23:46:44 +0300

    lint

--- src/bin/toFileContent.ts
+++ src/bin/toFileContent.ts
@@ -1,7 +1,13 @@
 export function toFileContent(x: string) {
   let s = x
-    .replace(/\s+(\r?\n *<\/?(script|link|head|body|html|meta|title)[> ])/g, "$1")
-    .replace(/\s+(\r?\n *<\/?(main|header|footer|nav|section|div|h\d|p|ul|ol|li|table)[> ])/g, "$1")
+    .replace(
+      /\s+(\r?\n *<\/?(script|link|head|body|html|meta|title)[> ])/g,
+      "$1",
+    )
+    .replace(
+      /\s+(\r?\n *<\/?(main|header|footer|nav|section|div|h\d|p|ul|ol|li|table)[> ])/g,
+      "$1",
+    )
     .trim();
 
   return `${s}\n`;
