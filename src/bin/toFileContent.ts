2c9bf961240c94ee7d88d97cef1b35afdb85f265
axtk <axtkx@inbox.ru>
2025-10-19 23:46:16 +0300

    remove more blank lines

--- src/bin/toFileContent.ts
+++ src/bin/toFileContent.ts
@@ -1,3 +1,8 @@
 export function toFileContent(x: string) {
-  return `${x.replace(/\s+(\r?\n *<\/?(script|link|head|body|html|meta|title)[> ])/g, "$1").trim()}\n`;
+  let s = x
+    .replace(/\s+(\r?\n *<\/?(script|link|head|body|html|meta|title)[> ])/g, "$1")
+    .replace(/\s+(\r?\n *<\/?(main|header|footer|nav|section|div|h\d|p|ul|ol|li|table)[> ])/g, "$1")
+    .trim();
+
+  return `${s}\n`;
 }
