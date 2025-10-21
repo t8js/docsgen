69986591b498f303a445ac93e7c148f0572c4d44
axtk <axtkx@inbox.ru>
2025-10-21 16:27:28 +0300

    fix content preprocessing

--- src/bin/parsing/preprocessContent.ts
+++ src/bin/parsing/preprocessContent.ts
@@ -10,23 +10,20 @@ const marker = {
 };
 
 export function preprocessContent(s: string) {
-  let k0 = s.length;
-  let k1 = s.length;
+  let k0 = -1;
+  let k1 = -1;
 
   while (
-    (k0 = s.lastIndexOf(marker.show.end, k1)) !== -1 &&
-    (k1 = s.lastIndexOf(marker.show.start, k0)) !== -1
+    (k1 = s.lastIndexOf(marker.show.end)) !== -1 &&
+    (k0 = s.lastIndexOf(marker.show.start, k1)) !== -1
   )
     s = `${s.slice(0, k0)}${s.slice(k0 + marker.show.start.length, k1)}${s.slice(k1 + marker.show.end.length)}`;
 
-  k0 = s.length;
-  k1 = s.length;
-
   while (
-    (k0 = s.lastIndexOf(marker.hide.end, k1)) !== -1 &&
-    (k1 = s.lastIndexOf(marker.hide.start, k0)) !== -1
+    (k1 = s.lastIndexOf(marker.hide.end)) !== -1 &&
+    (k0 = s.lastIndexOf(marker.hide.start, k1)) !== -1
   )
-    s = `${s.slice(0, k0 + marker.show.start.length - 1)}${s.slice(k0 + marker.show.start.length, k1)}${s.slice(k1 + marker.show.end.length + 2)}`;
+    s = `${s.slice(0, k0 + marker.hide.start.length - 1)}${s.slice(k0 + marker.hide.start.length, k1)}${s.slice(k1 + 2)}`;
 
   return s;
 }
