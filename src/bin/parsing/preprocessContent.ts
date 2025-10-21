1b384dcaf67f93dead753d1a224e4f65a36b8f1d
axtk <axtkx@inbox.ru>
2025-10-21 16:05:52 +0300

    fix content preprocessing

--- src/bin/parsing/preprocessContent.ts
+++ src/bin/parsing/preprocessContent.ts
@@ -10,21 +10,21 @@ const marker = {
 };
 
 export function preprocessContent(s: string) {
-  let k0 = -1;
-  let k1 = -1;
+  let k0 = s.length;
+  let k1 = s.length;
 
   while (
-    (k0 = s.indexOf(marker.show.start, k1)) !== -1 &&
-    (k1 = s.indexOf(marker.show.end, k0)) !== -1
+    (k0 = s.lastIndexOf(marker.show.end, k1)) !== -1 &&
+    (k1 = s.lastIndexOf(marker.show.start, k0)) !== -1
   )
     s = `${s.slice(0, k0)}${s.slice(k0 + marker.show.start.length, k1)}${s.slice(k1 + marker.show.end.length)}`;
 
-  k0 = -1;
-  k1 = -1;
+  k0 = s.length;
+  k1 = s.length;
 
   while (
-    (k0 = s.indexOf(marker.hide.start, k1)) !== -1 &&
-    (k1 = s.indexOf(marker.hide.end, k0)) !== -1
+    (k0 = s.lastIndexOf(marker.hide.end, k1)) !== -1 &&
+    (k1 = s.lastIndexOf(marker.hide.start, k0)) !== -1
   )
     s = `${s.slice(0, k0 + marker.show.start.length - 1)}${s.slice(k0 + marker.show.start.length, k1)}${s.slice(k1 + marker.show.end.length + 2)}`;
 
