872cebec17afc68ecdac7264bc89b0a829f54ce9
axtk <axtkx@inbox.ru>
2025-11-19 22:42:42 +0300

    update typography tweak

--- src/bin/setContent.ts
+++ src/bin/setContent.ts
@@ -30,7 +30,7 @@ function getDefaultCodeStyleContent(cssRoot: string) {
 
 function tweakTypography(s = "") {
   return s
-    .replace(/\b(for|in|on|at|with|a|an|the)\s+/gi, "$1\xa0")
+    .replace(/\b(for|in|on|at|to|with|a|an|the|its)\s+/gi, "$1\xa0")
     .replace(/\b(React)\s+(apps?)\b/gi, "$1\xa0$2");
 }
 
