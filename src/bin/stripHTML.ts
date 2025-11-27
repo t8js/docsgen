095403887a2f2d1ae67e2839a228c3699a521831
axtk <axtkx@inbox.ru>
2025-11-27 16:25:42 +0300

    minor update

--- src/bin/stripHTML.ts
+++ src/bin/stripHTML.ts
@@ -3,7 +3,6 @@ import { JSDOM } from "jsdom";
 export function stripHTML(content: string, replaceNbsp = false) {
   try {
     let t = content.replaceAll("<sup>", " (").replaceAll("</sup>", ")");
-
     let s = new JSDOM(t).window.document.body.textContent;
 
     if (replaceNbsp) s = s.replaceAll("\xa0", " ");
