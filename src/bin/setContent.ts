2cca29bd7f8c95394991d705ee165bc6430c8306
axtk <axtkx@inbox.ru>
2025-12-24 00:12:05 +0300

    fix light theme code block style url

--- src/bin/setContent.ts
+++ src/bin/setContent.ts
@@ -23,7 +23,7 @@ const __dirname = dirname(__filename);
 
 function getDefaultCodeStyleContent(cssRoot: string) {
   return `
-<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.11.1/styles/dark.min.css" media="(prefers-color-scheme: light)">
+<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.11.1/styles/stackoverflow-light.min.css" media="(prefers-color-scheme: light)">
 <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.11.1/styles/base16/material.min.css" media="(prefers-color-scheme: dark)">
 <link rel="stylesheet" href="${cssRoot}/code.css">
 <script src="https://unpkg.com/@highlightjs/cdn-assets@11.11.1/highlight.min.js"></script>
