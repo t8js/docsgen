587d4c1d3f00a8c4853f3c90eea2d7582ba25af3
axtk <axtkx@inbox.ru>
2026-02-05 16:13:15 +0300

    cache fetched content

--- src/bin/fetchContent.ts
+++ src/bin/fetchContent.ts
@@ -1,18 +1,28 @@
 import { readFile } from "node:fs/promises";
 
+let cachedContent = new Map<string, string>();
+
 export async function fetchContent(location: string | undefined) {
   if (!location) return "";
 
+  let content = cachedContent.get(location);
+
+  if (content !== undefined) return content;
+
   if (/^https?:\/\//.test(location)) {
     try {
-      return await (await fetch(location, { cache: "no-cache" })).text();
+      content = await (await fetch(location, { cache: "no-cache" })).text();
     } catch {
       console.warn(`Failed to fetch content from '${location}'`);
-      return "";
     }
   }
+  else {
+    let locationPath = location.replace(/^\//, "");
 
-  let locationPath = location.replace(/^\//, "");
+    content = (await readFile(locationPath)).toString();
+  }
 
-  return (await readFile(locationPath)).toString();
+  cachedContent.set(location, content ??= "");
+
+  return content;
 }
